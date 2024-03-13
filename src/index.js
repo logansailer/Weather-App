26d662b5e55d2a6372bda366349e6b1bb8ad3f08
Logan Sailer <logansailer0633@gmail.com>
2024-03-13 14:38:09 +0900

    testing to see if webpack works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles.css';
+import "./styles.css";
+
+/*
+set default data for app
+get data fromm user
+send it to api request
+get data I want
+display it on the page
+*/
+
+const location = document.querySelector("#location").value;
+
+console.log(location);
+console.log("text");
