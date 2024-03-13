f86b424adb0d9ef77879904ad8bc58b5e7352472
Logan Sailer <logansailer0633@gmail.com>
2024-03-13 16:19:04 +0900

    added js to retrieve weather API

--- src/index.js
+++ src/index.js
@@ -7,8 +7,20 @@ send it to api request
 get data I want
 display it on the page
 */
-
 const location = document.querySelector("#location").value;
+const today = document.querySelector("#today");
+const tempToday = document.querySelector("#temp-today");
 
-console.log(location);
-console.log("text");
+async function getWeatherData(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=7bf9ad50d4414a82a2024440241203&q=${location}`,
+    {
+      mode: "cors",
+    }
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+function renderPage() {}
+
+getWeatherData(location);
