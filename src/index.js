66e3dd91f5893609d86d0e400dd833afd11a3a73
Logan <logansailer0633@gamil.com>
2024-03-13 22:50:44 +0900

    stored data from.json to variables

--- src/index.js
+++ src/index.js
@@ -6,11 +6,36 @@ get data fromm user
 send it to api request
 get data I want
 display it on the page
+creative idea SUNSCREEN: calculate sunscreen with UV data
 */
 const location = document.querySelector("#location").value;
 const today = document.querySelector("#today");
 const tempToday = document.querySelector("#temp-today");
 
+//uses variables to render data on page
+function renderPage() {}
+
+//processes weather data to usable variables
+function weatherProcessor(weatherData) {
+  const usableData = {
+    condition: weatherData.current.condition.text,
+    feelsLikeF: weatherData.current.deelslike_f,
+    tempF: weatherData.current.temp_f,
+    uv: weatherData.current.uv,
+    windMPH: weatherData.current.wind_mph,
+    location: weatherData.location.name.toUpperCase(),
+  };
+
+  if (weatherData.location.country === "United States of America") {
+    usableData.region = weatherData.location.region.toUpperCase();
+  } else {
+    usableData.region = weatherData.location.country.toUpperCase();
+  }
+
+  return usableData;
+}
+
+//fetches weather data from api and sends to other functions for display/processing
 async function getWeatherData(location) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=7bf9ad50d4414a82a2024440241203&q=${location}`,
@@ -20,7 +45,8 @@ async function getWeatherData(location) {
   );
   const weatherData = await response.json();
   console.log(weatherData);
+  const processedWeather = weatherProcessor(weatherData);
+  renderPage(processedWeather);
 }
-function renderPage() {}
 
 getWeatherData(location);
