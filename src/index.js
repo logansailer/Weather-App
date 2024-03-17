a4a40acf6383d51276d20ddf08b3e81af568c70d
Logan <logansailer0633@gamil.com>
2024-03-17 21:57:48 +0900

    added backgound and finished logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import "./styles.css";
-/*
-creative idea SUNSCREEN: calculate sunscreen with UV data
-*/
 
-/* need to do:
-fix bug where time stops working after 12
-dynamically add weather in seperate function */
-
+//determines if sunscreen is needed based on epa.gov's recommendations
 function sunWarning(uv) {
-  console.log(uv);
+  if (uv === 0) {
+    return "You don't need to apply sunscreen right now, but this may change later in the day";
+  } else if (0 < uv && uv <= 2.9) {
+    return "Sunscreen may not be needed at the moment. Radiation is low enough that you can safely go outside without protection, but sun protection should still be considered. This may change throughout the day.";
+  } else if (2.9 < uv && uv <= 5) {
+    return "You should apply sunscreen before going outside. Seek shade when sun is most intense.";
+  } else if (5 < uv && uv <= 10) {
+    return "Sunscreen is necessary before going outside. Protective clothing and sunglasses may also be necessary. Reapply sunscreen often.";
+  } else if (10 < uv && uv <= 11) {
+    return "Going outside for long periods is ill-advised. If outside, sunscreen and sun protective clothing are absolutely necessary.";
+  }
 }
 
 //renders current weather info to the DOM
@@ -38,6 +42,14 @@ function renderWeather(weatherObject) {
   }
 }
 
+function checkHour(currentTime, timeAdded) {
+  if (currentTime + timeAdded > 23) {
+    return currentTime;
+  } else {
+    return currentTime + timeAdded;
+  }
+}
+
 //processes current weather data to usable variables
 function weatherProcessor(weatherData) {
   const currentTime = new Date(
@@ -50,12 +62,13 @@ function weatherProcessor(weatherData) {
     uv: weatherData.current.uv,
     windMPH: Math.round(weatherData.current.wind_mph),
     location: weatherData.location.name.toUpperCase(),
+    //checks if data can be pulled from forecast data (within 24 hours of start)
     hour: {
-      1: weatherData.forecast.forecastday[0].hour[currentTime + 1],
-      2: weatherData.forecast.forecastday[0].hour[currentTime + 2],
-      3: weatherData.forecast.forecastday[0].hour[currentTime + 3],
-      4: weatherData.forecast.forecastday[0].hour[currentTime + 4],
-      5: weatherData.forecast.forecastday[0].hour[currentTime + 5],
+      1: weatherData.forecast.forecastday[0].hour[checkHour(currentTime, 1)],
+      2: weatherData.forecast.forecastday[0].hour[checkHour(currentTime, 2)],
+      3: weatherData.forecast.forecastday[0].hour[checkHour(currentTime, 3)],
+      4: weatherData.forecast.forecastday[0].hour[checkHour(currentTime, 4)],
+      5: weatherData.forecast.forecastday[0].hour[checkHour(currentTime, 5)],
     },
   };
   //sets region based on in US vs outside US
